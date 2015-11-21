030c2c28466a9b5ac8355309e3dffade073dd503
andrew <synportack24@gmail.com>
2015-11-21 09:39:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/andrew/gpio/com/example/android/gpio/fragments/sysfstempurature_fragment.java
@@ -0,0 +1,25 @@
+package com.example.andrew.gpio.com.example.android.gpio.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.andrew.gpio.R;
+
+/**
+ * Created by andrew on 11/21/15.
+ */
+public class sysfstempurature_fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        return super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_sysfstempurature, container, false);
+        return  rootView;
+    }
+
+}
